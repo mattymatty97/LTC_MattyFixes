3ffb6c3210e2c24fb365b92ff85af68ddcd7ba2a
mattymatty <matteo.francesconi@freemails.it>
2024-07-21 22:00:34 +0200

    Cleanup

--- Plugin/src/MattyFixes.cs
+++ Plugin/src/MattyFixes.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using BepInEx;
-using BepInEx.Bootstrap;
 using BepInEx.Logging;
 using HarmonyLib;
 using MattyFixes.Dependency;
-using MattyFixes.PopUp;
-using PluginInfo = BepInEx.PluginInfo;
 
 namespace MattyFixes;
 
